e8e111c56bfe3c0623c9b88f15466f2e5a3390b1
Victor <vmartinez@machina.ai>
2020-11-03 14:59:56 -0600

    network definition

--- model.py
+++ model.py
@@ -15,8 +15,21 @@ class QNetwork(nn.Module):
         """
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
-        "*** YOUR CODE HERE ***"
+
+        super().__init__()
+        # Defining the layers, 128, 64, 10 units each
+        self.fc1 = nn.Linear(state_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        # Output layer, 10 units - one for each digit
+        self.fc3 = nn.Linear(64, action_size)
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        pass
+        x = self.fc1(state)
+        x = F.relu(x)
+        x = self.fc2(state)
+        x = F.relu(x)
+        x = self.fc3(state)
+        x = F.softmax(x, dim=1)
+        
+        return x
